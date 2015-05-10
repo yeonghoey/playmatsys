9ec66c9f68c640a5b9cd62f7f707c7143a57da73
hoey <nznial@gmail.com>
2015-05-10 18:07:23 +0900

    working with accordion

--- .vimrc
+++ .vimrc
@@ -1,7 +1,9 @@
 au BufRead,BufNewFile *.kv set syntax=kivy
 
 let @k='eval_main.py'
+set tags+=/Applications/Kivy.app/Contents/Resources/kivy/kivy/tags
 
 "~/.vim/after/compiler/kivy.vim
 let g:dispatch_compilers = {'kivy': 'kivy'}
+
 nnoremap <F10> :update %<CR>:Dispatch kivy <C-R>k<CR>
--- eval_main.py
+++ eval_main.py
@@ -12,29 +12,15 @@ from kivy.garden.graph import Graph, MeshLinePlot, MeshStemPlot, SmoothLinePlot
 from math import sin
 import scipy.stats
 
-class EvalScreen(Screen):
-    graph = ObjectProperty(None)
+class EvalLayout(BoxLayout):
+    acc = ObjectProperty(None)
 
 class EvalApp(App):
     def build(self):
-        self.go_next_screen()
+        acc = self.root.acc
+        acc.add_widget(Builder.load_file('scenes/prepare.kv'))
+        #acc.select(acc.children[-1])
 
-#        graph = self.root.ids.graph
-#        graph.xlabel = 'X'
-#        graph.ylabel = 'Y'
-#        graph.x_ticks_minor = 5
-#        graph.x_ticks_major = 10
-#        graph.y_ticks_major = 0.1
-#        graph.y_grid_label = True
-#        graph.x_grid_label = True
-#        graph.padding = 5
-#        graph.x_grid = True
-#        graph.y_grid = True
-#        graph.xmin = 0
-#        graph.xmax = 100
-#        graph.ymin = 0
-#        graph.ymax = 0.1
-#       
 #        points = [(x, scipy.stats.norm(50, 16.3).pdf(x)) for x in range(0, 100)]
 #
 #        plot = SmoothLinePlot(color=[1, 0, 0, 1])
@@ -44,22 +30,22 @@ class EvalApp(App):
 #        graph.add_plot(plot)
 #        graph.add_plot(plotsub)
 #
-    def load_screen(self, name):
-        screen = Builder.load_file(name.lower())
-        graph = screen.graph
-
-        points = [(x, scipy.stats.norm(50, 16.3).pdf(x)) for x in range(0, 100)]
-        plot = MeshLinePlot(color=[1, 0, 0, 1])
-
-        plot.points = points
-        graph.ymax = 0.1
-        graph.add_plot(plot)
-        return screen
-
-    def go_next_screen(self):
-        screen = self.load_screen('screens/before.kv')
-        sm = self.root.ids.sm
-        sm.add_widget(screen)
-
+#    def load_screen(self, name):
+#        screen = Builder.load_file(name.lower())
+#        graph = screen.graph
+#
+#        points = [(x, scipy.stats.norm(50, 16.3).pdf(x)) for x in range(0, 100)]
+#        plot = MeshLinePlot(color=[1, 0, 0, 1])
+#
+#        plot.points = points
+#        graph.ymax = 0.1
+#        graph.add_plot(plot)
+#        return screen
+#
+#    def go_next_screen(self):
+#        screen = self.load_screen('screens/before.kv')
+#        sm = self.root.ids.sm
+#        sm.add_widget(screen)
+#
 if __name__ == '__main__':
     EvalApp().run()
